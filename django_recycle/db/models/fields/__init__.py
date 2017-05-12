a7dfa31761a6598de52d4952b4ab7d7eef79ca1b
Zoltan Szalai <defaultdict@gmail.com>
2017-05-12 22:08:52 +0200

    models.SubfieldBase django backward compat

--- django_recycle/db/models/fields/__init__.py
+++ django_recycle/db/models/fields/__init__.py
@@ -1,15 +1,25 @@
+import django
 from django.db import models
 
 
-class NullableCharField(models.CharField):
+if django.VERSION < (1, 8):
+    base_field_class = six.with_metaclass(models.SubfieldBase, models.CharField)
+else:
+    base_field_class = models.CharField
+
+
+class NullableCharField(base_field_class):
     description = "CharField that stores NULL but returns ''"
     # this ensures to_python will be called
-    __metaclass__ = models.SubfieldBase
+    # __metaclass__ = models.SubfieldBase
 
     def to_python(self, value):
         if isinstance(value, models.CharField):
             return value
         return value or ''
+
+    def from_db_value(self, value, expression, connection, context):
+        return self.to_python(value)
 
     def get_prep_value(self, value):
         return value or None
