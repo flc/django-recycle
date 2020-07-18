8f5c190178fe7259a50198a76a72d300e080d191
Zoltan Szalai <flack@mcule.com>
2020-07-18 23:57:41 +0200

    mimetype -> content_type; eliminate RequestContext

--- django_recycle/utils/pdf.py
+++ django_recycle/utils/pdf.py
@@ -9,18 +9,18 @@ import pdfkit
 
 def render_to_pdf_pdfkit(request, template_name, context, filename, extra_options=None):
     template = get_template(template_name)
-    html = template.render(RequestContext(request, context))
+    html = template.render(context, request=request)
 
     tmp_file = tempfile.NamedTemporaryFile(prefix='pdf_output_{}')
     options = {
-        'encoding': "UTF-8",
+        'encoding': 'UTF-8',
         'footer-right': 'Page [page] of [toPage]',
         'margin-top': '0.5cm',
         'margin-right': '0.5cm',
         'margin-bottom': '0.5cm',
         'margin-left': '0.5cm',
         # 'footer-spacing': '-0.2',
-        }
+    }
     extra_options = extra_options or {}
     options.update(extra_options)
 
@@ -28,7 +28,7 @@ def render_to_pdf_pdfkit(request, template_name, context, filename, extra_option
     tmp_file.seek(0)
     pdf = tmp_file.read()
 
-    response = HttpResponse(mimetype="application/pdf")
+    response = HttpResponse(content_type='application/pdf')
     response.write(pdf)
     if filename is not None:
         response['Content-Disposition'] = 'attachment; filename={}'.format(filename)
--- django_recycle/weasyprint/utils.py
+++ django_recycle/weasyprint/utils.py
@@ -15,29 +15,33 @@ import weasyprint
 from .url_fetcher import static_file_url_fetcher
 
 
-def render_to_pdf(request,
-                  template_name,
-                  context,
-                  filename=None,
-                  url_fetcher=static_file_url_fetcher,
-                  ):
+def render_to_pdf(
+    request,
+    template_name,
+    context,
+    filename=None,
+    url_fetcher=static_file_url_fetcher,
+):
     template = get_template(template_name)
-    html = template.render(RequestContext(request, context))
-    response = HttpResponse(mimetype="application/pdf")
-    base_url = request.build_absolute_uri("/")
-    weasyprint.HTML(string=html,
-                    base_url=base_url,
-                    url_fetcher=url_fetcher).write_pdf(response)
+    html = template.render(context, request=request)
+    response = HttpResponse(content_type='application/pdf')
+    base_url = request.build_absolute_uri('/')
+    weasyprint.HTML(
+        string=html,
+        base_url=base_url,
+        url_fetcher=url_fetcher
+    ).write_pdf(response)
     if filename is not None:
         response['Content-Disposition'] = 'attachment; filename={}'.format(filename)
     return response
 
 
-def generate_pdf(template_name,
-                 context,
-                 file_object=None,
-                 url_fetcher=static_file_url_fetcher,
-                 ):
+def generate_pdf(
+    template_name,
+    context,
+    file_object=None,
+    url_fetcher=static_file_url_fetcher
+):
     if not file_object:
         file_object = IO_class()
     if not context:
