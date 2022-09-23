7a5c1b320782da446f75a0de89ef9ce5f247af1e
Zoltán Szalai <flack@mcule.com>
2022-09-23 20:49:30 +0200

    update large qs iterator functions

--- django_recycle/utils/queryset.py
+++ django_recycle/utils/queryset.py
@@ -1,7 +1,7 @@
 import gc
 
 
-def large_qs_pk_iterator(queryset, size=10000):
+def large_qs_pk_iterator(queryset, size=10000, use_iterator_method=False):
     """Iterate over a Django Queryset ordered by the primary key
 
     This method loads a maximum of chunksize (default: 10000) rows in it's
@@ -13,58 +13,55 @@ def large_qs_pk_iterator(queryset, size=10000):
     ordered query sets.
     """
     queryset = queryset.order_by("pk")
-    pk = 0
+    pk = None
     while True:
-        yielded = False
-        for row in queryset.filter(pk__gt=pk)[:size].iterator():
+        qs = queryset
+        if pk:
+            qs = qs.filter(pk__gt=pk)
+        qs = qs[:size]
+        if use_iterator_method:
+            qs = qs.iterator()
+        idx = None
+        for idx, row in enumerate(qs, 1):
             # keep this before yield because the row (if it is a model object)
             # may gets deleted
             pk = row.pk
-            yielded = True
             yield row
         gc.collect()
-        if not yielded:
+        if idx < size:
             break
 
 
-def large_vlqs_pk_iterator(queryset, size=50000):
+def large_vlqs_pk_iterator(queryset, size=50000, use_iterator_method=False):
     """Use for iterating over large ValuesListQuerySet's.
     Note that the implementation of the iterator does not support
     ordered query sets."""
-    if not hasattr(queryset, 'flat'):
-        yield from queryset.iterator(chunk_size=size)
-    else:
-        pk_inserted = False
-        original_flat = queryset.flat
-        if queryset.field_names[0] not in ("id", "pk"):
-            values = queryset.field_names[:]
-            values.insert(0, "pk")
-            queryset = queryset.values_list(*values)
-            pk_inserted = True
+    fields = getattr(queryset, '_fields', [])
+    if fields and fields.index('pk') != 0:
+        raise ValueError("The `pk` field must be included as the first field.")
 
-        queryset = queryset.order_by("pk")
-        pk = 0
-        while True:
-            yielded = False
-            for row in queryset.filter(pk__gt=pk)[:size].iterator():
-                yielded = True
-                if pk_inserted:
-                    if original_flat:
-                        yield row[-1]
-                    else:
-                        yield row[1:]
-                else:
-                    yield row
-            if not yielded:
-                break
-            if isinstance(row, tuple):
-                pk = row[0]
-            else:  # flat=True case
-                pk = row
-            gc.collect()
+    queryset = queryset.order_by("pk")
+    pk = None
+    while True:
+        qs = queryset
+        if pk:
+            qs = qs.filter(pk__gt=pk)
+        qs = qs[:size]
+        if use_iterator_method:
+            qs = qs.iterator()
+        idx = 0
+        for idx, row in enumerate(qs, 1):
+            yield row
+        if idx < size:
+            break
+        if isinstance(row, tuple):
+            pk = row[0]
+        else:  # flat=True case
+            pk = row
+        gc.collect()
 
 
-def large_qs_iterator(queryset, size=50000):
+def large_qs_iterator(queryset, size=50000, use_iterator_method=False):
     """Use for iterating over large Querysets.
     Do not use on a queryset that may change during the iteration and only
     use for ordered querysets. If ordering doesn't matter use
@@ -73,9 +70,12 @@ def large_qs_iterator(queryset, size=50000):
     while True:
         idx = -1
         limit = offset + size
-        for idx, row in enumerate(queryset[offset:limit].iterator(), 1):
+        qs = queryset[offset:limit]
+        if use_iterator_method:
+            qs = qs.iterator()
+        for idx, row in enumerate(qs, 1):
             yield row
         offset += idx
-        if idx == -1:
+        if idx < size:
             break
         gc.collect()
